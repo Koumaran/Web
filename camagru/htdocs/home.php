8b363cf1c68f994af9bbdd35e47ccae94f7a0ffc
Jegathas SIVANESAN <jsivanes@e2r5p9.42.fr>
2017-06-05 11:00:32 +0200

    debut du projet

---
+++ camagru/htdocs/home.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<HTML xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
+	<HEAD>
+	<META charset="UTF-8">
+	<LINK rel="stylesheet" media="screen" type="text/css" title="style" href="css/main_style.css">
+	<TITLE>Camagru</TITLE>
+	</HEAD>
+	<BODY>
+		<header>
+			<h1>Camagru</h1>
+			<div id="log_point">
+				<img id="login_img" src="ressources/login2.png" alt="Longin" title="Se connecter">
+			</div>
+		</header>
+		<DIV classe="body">
+
+		</DIV>
+		<DIV classe="footer">
+			
+		</DIV>
+	</BODY>
+</HTML>
